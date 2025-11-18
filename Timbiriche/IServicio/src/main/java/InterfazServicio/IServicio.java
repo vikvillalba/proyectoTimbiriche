cb4c4ce1143bd4718aaf88b1943a959a96db00fb
JackMurrieta <jack.murrieta252851@potros.itson.edu.mx>
2025-11-17 19:12:35 -0700

    EventoPaqueteDTO e IServicio

---
+++ Timbiriche/IServicio/src/main/java/InterfazServicio/IServicio.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package InterfazServicio;
+
+/**
+ *
+ * @author Jack Murrieta
+ */
+public interface IServicio {
+    
+
+    public int getPuerto();
+
+    public String getHost();
+
+}
